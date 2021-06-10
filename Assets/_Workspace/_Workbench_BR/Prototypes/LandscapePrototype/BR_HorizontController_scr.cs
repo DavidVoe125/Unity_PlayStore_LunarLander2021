118bacbf5d99d61391a7d0338007f32aa52f2c6f
brubow@newvisionsproject.de <brubow@newvisionsproject.de>
2021-06-10 16:47:57 +0200

    Final Version of first Lander Steering Prototyp.

---
+++ Assets/_Workspace/_Workbench_BR/Prototypes/LandscapePrototype/BR_HorizontController_scr.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BR_HorizontController_scr : MonoBehaviour
+{
+    private Quaternion rot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rot = transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = rot;
+    }
+}
